16fb0f614370a8389263270eace1107e9e9bf496
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-12 23:02:38 +0200

    Update #03#Net_Check_RabbitMQ_Ports_On_Remote_Server.ps1

--- Network Scripts/#03#Net_Check_RabbitMQ_Ports_On_Remote_Server.ps1
+++ Network Scripts/#03#Net_Check_RabbitMQ_Ports_On_Remote_Server.ps1
@@ -1,4 +1,4 @@
-﻿# Check RabbitMQ on remote server
+# Check RabbitMQ on remote server
 $IP_Address = "192.168.100.121"
 
 # Check RabbitMQ port (Not encrypted)
@@ -7,5 +7,8 @@ New-Object System.Net.Sockets.TcpClient($IP_Address,5672)
 # Check Management and monitoring of RabbitMQ
 New-Object System.Net.Sockets.TcpClient($IP_Address,15672)
 
-# Check Used for inter-node and CLI tools communication
+# Used for inter-node and CLI tools communication
 New-Object System.Net.Sockets.TcpClient($IP_Address,25672)
+
+# Used for peer discovery service by RabbitMQ nodes and CLI tools
+New-Object System.Net.Sockets.TcpClient($IP_Address,4369)
