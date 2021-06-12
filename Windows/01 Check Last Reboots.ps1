0bdb1f39efb58c70e9163e195703cc12982700fa
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-13 00:05:40 +0200

    Add files via upload

---
+++ Windows/01 Check Last Reboots.ps1
@@ -0,0 +1,14 @@
+﻿# Scripts that check the system log for event id 1074 "Last restarts of the system. 
+# Output file in c: root "LastReboot.txt"
+# 
+# Event ID Description
+#
+# 1074 System has been shutdown by a process/user
+#
+# 6008 The previous system shutdown at time on date was unexpected
+#
+# 6013 The system uptime is number seconds 
+
+$Procs = Get-WinEvent -FilterHashtable @{logname = 'System'; id = 1074,6008} | Format-Table -wrap 
+    Out-File -FilePath "C:\LastReboot.txt" -InputObject $Procs -Encoding ASCII -Width 300
+        $Procs
