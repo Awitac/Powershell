1e99ee320ec909413e8f7b30db71fe538bf69d5b
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-13 00:08:42 +0200

    Update 01 Check Last Reboots.ps1

--- Windows/01 Check Last Reboots.ps1
+++ Windows/01 Check Last Reboots.ps1
@@ -1,4 +1,4 @@
-# Scripts that check the system log for event id 1074, 6008 and 6013 
+# Scripts that check the system log for event id 1074 and 6008  
 # Output file in c: root "LastReboot.txt"
 # 
 # Event ID Description
@@ -7,8 +7,7 @@
 #
 # 6008 The previous system shutdown at time on date was unexpected
 #
-# 6013 The system uptime is number seconds 
 
-$Procs = Get-WinEvent -FilterHashtable @{logname = 'System'; id = 1074,6008,6013} | Format-Table -wrap 
+$Procs = Get-WinEvent -FilterHashtable @{logname = 'System'; id = 1074,6008} | Format-Table -wrap 
     Out-File -FilePath "C:\LastReboot.txt" -InputObject $Procs -Encoding ASCII -Width 300
         $Procs
