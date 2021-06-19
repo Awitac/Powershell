c6368f50da4971710ea0da1f8866111c37321417
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-20 00:51:17 +0200

    Create 03 Create RabbitMQ config file.ps1

---
+++ RabbitMQ/03 Create RabbitMQ config file.ps1
@@ -0,0 +1,27 @@
+ # Create RabbitMQ configuration file with pause minority
+New-Item -Path 'C:\ProgramData\RabbitMQ\rabbitmq.conf' -ItemType File -Force
+Write-Host "The file [$file] has been created."
+
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## Clustering"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ====================="
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "cluster_partition_handling = autoheal"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## 5671 SSL Encrytion"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ====================="
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## listeners.ssl.default = 5671"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## listeners.tcp"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ssl_options.verify = verify_peer"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ssl_options.fail_if_no_peer_cert = false"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ssl_options.cacertfile = C:\\certs\\ca_certificate.pem"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ssl_options.certfile = C:\\certs\\server_certificate.pem"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "## ssl_options.keyfile = C:\\certs\\server_key.pem"
+Add-Content C:\ProgramData\RabbitMQ\rabbitmq.conf "##"
+
+
+# Copy rabbitmq.conf in Documents folder
+Copy-Item 'C:\ProgramData\RabbitMQ\rabbitmq.conf' -destination $ENV:UserProfile\Documents -Force
+Write-Host "The file [$file] has been copied.
