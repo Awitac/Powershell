2e47e3eff3218e37a432e02ceba119214fab6bcd
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-19 16:09:11 +0200

    Update Awitac hail mary - force stop all services v1.1.ps1

--- Unite/Services/Awitac hail mary - force stop all services v1.1.ps1
+++ Unite/Services/Awitac hail mary - force stop all services v1.1.ps1
@@ -1,6 +1,11 @@
 # Creatated by Awitac 
 # Changes                                                                              ES 2021-06-16 
 # 
+#                                   ###### WARNING ######
+#      This script should only be run if you encounter issue with the default PS uninstallation
+#      Lets the PS un installation window roll back the system, when it is done, run the script
+#      then run the default PS uninstallation 
+#
 #######################################################################################################
 #
 # STOP Ascom IIS Sites
