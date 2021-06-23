a4bf8735cf0f9fab98a21de65048b38c936821fd
Awitac <85807607+Awitac@users.noreply.github.com>
2021-06-23 20:15:03 +0200

    Update Display Installed Windows Hotfix.ps1

--- Windows/Display Installed Windows Hotfix.ps1
+++ Windows/Display Installed Windows Hotfix.ps1
@@ -1,2 +1,9 @@
+# Creatated by Awitac 
+# Changes                                                         ES/SA 2021-06-19 
+###################################################################################
+#
+#  List Installed Windows Hotfixes
+# 
+##################################################################################
 # Listing Installed Hotfixes
 Get-CimInstance -ClassName Win32_QuickFixEngineering | Sort-Object -Property InstalledOn -Descending
