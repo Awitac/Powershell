82d80e363afe6a3e44acecb5eab387e1fde55cab
Awitac <85807607+Awitac@users.noreply.github.com>
2022-05-11 18:33:20 +0200

    Update Unite Axess Windows Firewall Inbound Rules.ps1

--- Unite/Unite Platform Firewall/Unite Axess Windows Firewall Inbound Rules.ps1
+++ Unite/Unite Platform Firewall/Unite Axess Windows Firewall Inbound Rules.ps1
@@ -8,10 +8,6 @@
 #    Unite TCP   444     Unite Axess (https) Shared PS / Axess server
 #    Unite TCP   29911   Unite Axess Websocket
 #
-# IP Ports for Unite Axess Server Outbound
-#    Unite TCP   443     Unite Axess (https) Standalone Axess server
-#    Unite TCP   444     Unite Axess (https) Shared PS / Axess server
-#
 #            NOTE: Depending on the Axess installation remove rule 443 or 444   
 #
 #            NO GARANTIES! ALWAYS CONFIRM THE RULES AFTER RUNNING THE SCRIPT
