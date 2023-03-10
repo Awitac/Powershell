6261f99d703fdefdbefacd77c72528df50edecc8
Awitac <85807607+Awitac@users.noreply.github.com>
2023-03-10 23:39:45 +0100

    Delete Display_SSL_Certificate_bindings.ps1

--- IIS/Display_SSL_Certificate_bindings.ps1
+++
@@ -1,7 +0,0 @@
-# Creatated by Awitac 
-# Changes                                                                              ES 2021-06-16 
-# 
-#######################################################################################################
-#
-# Display SSL Certificate bindings with netsh
-netsh http show sslcert 
